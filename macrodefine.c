1836ca0988e4568003294f1ca9c2f93b45823f11
JavierMV <javiermvia@gmail.com>
2019-02-14 22:19:56 +0100

    cambios

--- macrodefine.c
+++ macrodefine.c
@@ -7,7 +7,7 @@ int main()
     /* Define sustituye el texto , 3+2, de la macro CINCO en el resto del programa */
     printf("constante: %d \n", 3*CINCO);
     /* Constante es un dato que se guarda como tal en el código del programa */
-    printf("constante: %d \n", 3*CINCO);
+    printf("constante: %d \n", 32*CINCO);
     /* Cadenas alfanuméricas */
     printf("\x41\n");
     printf("\x4A\n");
